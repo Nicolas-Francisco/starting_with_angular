96e33d3c8354d264b16f990319ca1585a3ac58a1
Nicolás García <nicolas.garcia.r@ug.uchile.cl>
2021-12-09 13:27:49 -0300

    Se crea grilla de wojaks y se utilizan ngFor y ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { not } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
+import { Wojak } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +11,51 @@ export class AppComponent {
   public title = 'my-store';
   public img = 'https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Foriginal%2F000%2F031%2F671%2Fcover1.jpg';
   public b_disable = true;
+  public main_name = "BlackFire";
+
+  stands: string[] = ['Tusk', 'D4C', 'THE WORLD'];
+  new_stand = "";
 
   person = {
     name: 'BlackFire',
     age: '22',
     num_semesters: 11,
     my_semester: 8
-  }
+  };
+
+  wojaks: Wojak[] = [
+    {
+      name: "cryjak",
+      price: 99.9,
+      image: './assets/images/cryjak.jpg'
+    },
+    {
+      name: "darkjak",
+      price: 139.99,
+      image: './assets/images/darkjak.jpg'
+    },
+    {
+      name: "tradgirljak",
+      price: 199.9,
+      image: './assets/images/tradgirljak.jpg'
+    },
+    {
+      name: "twinkjak",
+      price: 56.99,
+      image: './assets/images/twinkjak.jpg'
+    }
+  ]
 
   public onClick() {
-    this.b_disable = !this.b_disable
+    this.b_disable = !this.b_disable;
+  }
+
+  public add_stand(){
+    this.stands.push(this.new_stand);
+    this.new_stand = "";
+  }
+
+  public delete_stand(index: number){
+    this.stands.splice(index, 1);
   }
 }
---
+++ src/app/product.model.ts
@@ -0,0 +1,5 @@
+export interface Wojak{
+  name: string;
+  price: number;
+  image: string;
+}
